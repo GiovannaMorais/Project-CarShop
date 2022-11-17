a2845c235f4bef61b1e61713f6543ad5e9f48fe7
GiovannaMorais <gigimorais9@hotmail.com>
2022-11-17 19:21:43 -0300

    Feat: update app

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import ErrorHandler from './Middlewares/ErrorHandler';
 import carRoutes from './routes/CarRoutes';
+import motorcyclesRoutes from './routes/MotorcycleRoutes';
 
 const app = express();
 app.use(express.json());
 app.use(carRoutes);
+app.use(motorcyclesRoutes);
 app.use(ErrorHandler.handle);
 
 export default app;
